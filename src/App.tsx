c13b76c2483f2f056596b62df31b82da31884491
Sokol Paja <kolpaja28@gmail.com>
2023-06-07 17:03:21 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'antd/dist/reset.css';
+import { Button } from 'antd';
+
+const App = () => {
+  return (
+    <Button type='dashed' className='font-bold text-blue-500'>
+      hi Mom
+    </Button>
+  );
+};
+
+export default App;
