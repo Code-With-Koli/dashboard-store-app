ed9b5044a42f4795a829bb05384e137e10e90a2a
Sokol Paja <kolpaja28@gmail.com>
2023-06-07 17:27:32 +0200

    try now bis

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import 'antd/dist/reset.css';
-import { Button } from 'antd';
+import { Image } from 'antd';
+import construction from './assets/under-construction.svg';
 
 const App = () => {
   return (
-    <Button type='dashed' className='font-bold text-blue-500'>
-      hi Mom
-    </Button>
+    <div className='flex flex-col justify-center w-full p-8'>
+      <h1 className='w-full p-4 text-lg font-bold text-center'>
+        Natural Stone Dashboard on construction{' '}
+      </h1>
+      <div className='flex justify-center items-center h-screen'>
+        <Image
+          src={construction}
+          alt='under construction'
+          width={500}
+          height={400}
+        />
+      </div>
+    </div>
   );
 };
 
